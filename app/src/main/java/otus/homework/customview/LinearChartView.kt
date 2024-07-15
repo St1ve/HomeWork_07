bb042a839d84a8d11a5a4e79d026c7b514589cdc
vrazdyakonov <razdyakonov.viktor@gmail.com>
2024-07-15 20:49:22 +0300

    Add pie chart and linear mock views

---
+++ app/src/main/java/otus/homework/customview/LinearChartView.kt
@@ -0,0 +1,24 @@
+package otus.homework.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class LinearChartView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+): View(context, attrs, defStyleAttr, defStyleRes) {
+
+    // FIXME: Implement
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    // FIXME: Implement
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+    }
+}
