3cd8414f951899d4a992dcf0f1c02014b873c1fe
vrazdyakonov <razdyakonov.viktor@gmail.com>
2024-07-20 11:49:41 +0300

    Add util for generating random color

--- app/src/main/java/otus/homework/customview/Utils.kt
+++ app/src/main/java/otus/homework/customview/Utils.kt
@@ -1,2 +1,11 @@
 package otus.homework.customview
 
+import android.graphics.Color
+
+fun generateRandomColor(): Int {
+    val r = (Math.random() * 256).toInt()
+    val g = (Math.random() * 256).toInt()
+    val b = (Math.random() * 256).toInt()
+
+    return Color.rgb(r, g, b)
+}
